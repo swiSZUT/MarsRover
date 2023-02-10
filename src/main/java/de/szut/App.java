b917cdd62d10ebf86927feaee4ad5ff767818f61
Stefan Wien <swi@webmen.de>
2023-02-10 10:53:25 +0100

    Initial commit

---
+++ src/main/java/de/szut/App.java
@@ -0,0 +1,13 @@
+package de.szut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
